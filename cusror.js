5e52d281869d36be46597f9e2d3a03c519dc73f4
Will Hanighen <144380313+WillHanighenSchool@users.noreply.github.com>
2024-03-28 17:51:29 -0400

    upload latest from school PC cuz i cant view the changes live while having the repo open ins VS code cuz school rules

---
+++ cusror.js
@@ -0,0 +1,17 @@
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  zIndex: '9999'
+
+  innerCursor.style.left = `${x}px`;
+  innerCursor.style.top = `${y}px`;
+  outerCursor.style.left = `${x}px`;
+  outerCursor.style.top = `${y}px`;
+
+}
+
